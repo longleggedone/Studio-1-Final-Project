e4afcc726a0cf69083e0620273a309aad939b00e
longleggedone <longlegsclassic@gmail.com>
2016-11-20 20:58:23 -0500

    First commit, adding all original files

---
+++ 3DProject02/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationController : MonoBehaviour {
+
+	public Animator anim;
+	public Rigidbody rb;
+
+	private float inputH;
+	private float inputV;
+
+	// Use this for initialization
+	void Start () {
+
+		anim = GetComponent<Animator> ();
+		rb = GetComponent<Rigidbody> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown("up")) 
+		{
+			anim.Play ("Walk", -1, 0.2f);
+		}
+	
+		if (Input.GetKeyDown("space")) 
+		{
+			anim.Play ("Jump", -1, 0f);
+		}
+
+
+		inputH = Input.GetAxis ("Horizontal");
+		inputV = Input.GetAxis ("Vertical");
+
+		anim.SetFloat("inputH", inputH);
+		anim.SetFloat("inputV", inputV);
+
+		float moveX = inputH * 20f * Time.deltaTime;
+		float moveZ = inputV * 50f * Time.deltaTime;
+
+		rb.velocity = new Vector3 (moveX, 0f, moveZ);
+
+			
+	}
+			
+}
